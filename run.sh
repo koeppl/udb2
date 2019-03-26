9a5e372bf939277c27f025131a8645f0f5ca20e4
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2019-03-26 19:21:32 +0900

    +rigtorp

--- run.sh
+++ run.sh
@@ -2,7 +2,8 @@
 make
 mkdir -p _logs
 # ls */run-test* | grep -v log | xargs -i echo "{} | sed \"s@RESULT @RESULT experiment={}@\" | tee {}.log 2>&1" | sh
-for ex in $(ls */run-test* | grep -v log); do
+# for ex in $(ls */run-test* | grep -v log); do
+for ex in c++11/run-test separate_chaining/run-test-cht separate_chaining/run-test-compact separate_chaining/run-test-plain sparsehash/run-test tsl_sparse_map/run-test sparsepp/run-test rigtorp/run-test; do
 	[[ -x $ex ]] || continue
 	name=$(dirname "$ex")
 	extension=$(basename "$ex" | sed 's@run-test@@')
@@ -11,4 +12,4 @@ for ex in $(ls */run-test* | grep -v log); do
 	# $ex | sed "s@RESULT @RESULT experiment=$name@" | tee _logs/$1.log 
 	$ex 2>&1 | sed "s@RESULT @RESULT experiment=$name @" | tee _logs/$name.log 
 done
-
+cat _logs/*.log | grep '^RESULT' > _logs/udb2.txt
