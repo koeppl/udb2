1f143e52dfc22be9ff09676a22e7ec9120d14e9c
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2019-03-29 16:33:59 +0900

    add compact sparse hash tables

--- run.sh
+++ run.sh
@@ -3,7 +3,9 @@ make
 mkdir -p _logs
 # ls */run-test* | grep -v log | xargs -i echo "{} | sed \"s@RESULT @RESULT experiment={}@\" | tee {}.log 2>&1" | sh
 # for ex in $(ls */run-test* | grep -v log); do
-for ex in c++11/run-test separate_chaining/run-test-cht separate_chaining/run-test-compact separate_chaining/run-test-plain sparsehash/run-test tsl_sparse_map/run-test sparsepp/run-test rigtorp/run-test; do
+
+
+for ex in c++11/run-test separate_chaining/run-test-cht separate_chaining/run-test-compact separate_chaining/run-test-plain sparsehash/run-test tsl_sparse_map/run-test sparsepp/run-test rigtorp/run-test compact_sparse_hash/run-test-cv-plain   compact_sparse_hash/run-test-elias-plain   compact_sparse_hash/run-test-layered-plain compact_sparse_hash/run-test-cv-sparse  compact_sparse_hash/run-test-elias-sparse  compact_sparse_hash/run-test-layered-sparse; do
 	[[ -x $ex ]] || continue
 	name=$(dirname "$ex")
 	extension=$(basename "$ex" | sed 's@run-test@@')
