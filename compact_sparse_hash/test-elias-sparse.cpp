2240493ad1ecd4d70cae7441123882e109884b90
Dominik Köppl <dominik.koeppl@uni-dortmund.de>
2019-03-29 16:39:58 +0900

    add files for compact_sparse_hash

---
+++ compact_sparse_hash/test-elias-sparse.cpp
@@ -0,0 +1,22 @@
+#include "../common.c"
+
+#include <tudocomp/util/compact_hash/map/typedefs.hpp>
+
+struct Hash32 {
+	inline size_t operator()(const uint32_t x) const {
+		return hash32(x);
+	}
+};
+
+void test_int(uint32_t n, uint32_t x0)
+{
+	using namespace tdc::compact_hash::map;
+	uint32_t i, x, z = 0;
+	sparse_elias_hashmap_t<uint32_t> h(32);
+	// spp::sparse_hash_map<uint32_t, uint32_t, Hash32> h;
+	for (i = 0, x = x0; i < n; ++i) {
+		x = hash32(x);
+		z += ++h[get_key(n, x)];
+	}
+	fprintf(stderr, "# unique keys: %ld; checksum: %u\n", h.size(), z);
+}
